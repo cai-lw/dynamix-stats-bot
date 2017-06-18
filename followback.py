3b02a7236e3724cb404190a02283aa202d9bce90
cai-lw <cai_lw@126.com>
2017-06-18 21:01:25 +0800

    Add automatic follow back

---
+++ followback.py
@@ -0,0 +1,16 @@
+import twitter
+import os
+
+api = twitter.Api(consumer_key=os.environ['CSMR_KEY'],
+                  consumer_secret=os.environ['CSMR_SCRT'],
+                  access_token_key=os.environ['ACSS_KEY'],
+                  access_token_secret=os.environ['ACSS_SCRT'])
+
+followback_ids = list(set(api.GetFollowerIDs()).difference(api.GetFriendIDs()))
+
+if len(followback_ids) > 0:
+    for batch in range((len(followback_ids) - 1) // 100 + 1):
+        for user in api.UsersLookup(user_id=followback_ids[batch * 100:(batch + 1) * 100]):
+            if not user.protected:
+                print('Followed @' + user.screen_name)
+                api.CreateFriendship(user.id)
